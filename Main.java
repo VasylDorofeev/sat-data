bb6123297ed98b094afef203bedf2c1ba86357f8
VasylDorofeev <vasyld2@nycstudents.net>
2024-06-11 14:09:37 +0000

    I found the total number of students who got a score below 1000

--- Main.java
+++ Main.java
@@ -25,17 +25,27 @@ public class Main {
     // make sure all data entered the ArrayList
     System.out.println(totalScore);
     System.out.println();
-    
+
+    // prints number of students who took their test
     System.out.println(totalScore.size() + " students took their test.");
     System.out.println();
 
-      int total = 0;
+    // prints the average sat score 
+    int total = 0;
     for(int score : totalScore){
       total += score;
     }
     double average = (double)total/totalScore.size();
     System.out.println(average);
-    
-  }
 
+    System.out.println();
+
+    int lowScore = 0;
+    for(int score : totalScore){
+      if(score >= 1000){
+      }
+      lowScore++;
+    }
+    System.out.println(lowScore+" students got a low score.");
+  }
 }
